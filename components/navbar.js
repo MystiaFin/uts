337f6e290bf28abd830fd44889e73c1d993fefcf
MystiaFin <cytrsx01@gmail.com>
2024-10-03 14:06:02 +0700

    menu on progress

--- components/navbar.js
+++ components/navbar.js
@@ -7,11 +7,11 @@
                 </div>
                 <div class="menu">
                     <ul>
-                        <li><a href="/home"><img src="/assets/navigations/home.svg"/>Home</a></li>
-                        <li><a href="../menu/menu/"><img src="/assets/navigations/menu.svg"/>Menu</a></li>
-                        <li><a href="#"><img src="/assets/navigations/order.svg"/>Orders</a></li>
-                        <li><a href="#"><img src="/assets/navigations/checkouts.svg"/>Checkouts</a></li>
-                        <li><a href="#"><img src="/assets/navigations/settings.svg"/>Settings</a></li>
+                        <li><a href="../home/home.html"><img src="/assets/navigations/home.svg"/>Home</a></li>
+                        <li><a href="../menu/menu.html"><img src="/assets/navigations/menu.svg"/>Menu</a></li>
+                        <li><a href="../order/order.html"><img src="/assets/navigations/order.svg"/>Orders</a></li>
+                        <li><a href="../checkouts/checkouts.html"><img src="/assets/navigations/checkouts.svg"/>Checkouts</a></li>
+                        <li><a href="../settings/settings.html"><img src="/assets/navigations/settings.svg"/>Settings</a></li>
                     </ul>
                 </div>
             </nav>`;
