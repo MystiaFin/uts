f7456cac249fe1ab8d2a24a0383448432de877c5
MystiaFin <cytrsx01@gmail.com>
2024-10-03 12:29:52 +0700

    updated component

--- components/navbar.js
+++ components/navbar.js
@@ -1,60 +1,22 @@
-class Navbar {
-    constructor(options) {
-      this.options = {
-        logoText: 'Logo',
-        menuItems: [
-          { text: 'Home', icon: '/assets/navigations/home.svg', href: '#' },
-          { text: 'Menu', icon: '/assets/navigations/menu.svg', href: '#' },
-          { text: 'Orders', icon: '/assets/navigations/order.svg', href: '#' },
-          { text: 'Checkouts', icon: '/assets/navigations/checkouts.svg', href: '#' },
-          { text: 'Settings', icon: '/assets/navigations/settings.svg', href: '#' }
-        ],
-        ...options
-      };
-      this.$el = null;
-    }
-  
-    render() {
-      this.$el = $('<nav>').addClass('sidebar');
-      
-      const $logo = $('<div>').addClass('logo').text(this.options.logoText);
-      this.$el.append($logo);
-  
-      const $menu = $('<div>').addClass('menu');
-      const $ul = $('<ul>');
-  
-      this.options.menuItems.forEach(item => {
-        const $li = $('<li>');
-        const $a = $('<a>').attr('href', item.href);
-        const $img = $('<img>').attr('src', item.icon);
-        $a.append($img).append(item.text);
-        $li.append($a);
-        $ul.append($li);
-      });
-  
-      $menu.append($ul);
-      this.$el.append($menu);
-  
-      return this.$el;
-    }
-  
-    mount($parent) {
-      $parent.append(this.render());
-    }
-  
-    setActive(index) {
-      this.$el.find('li').removeClass('active');
-      this.$el.find('li').eq(index).addClass('active');
-    }
-  }
-  
-  // Usage
-  $(document).ready(function() {
-    const navbar = new Navbar({
-      logoText: 'RedPOS'
-    });
-    navbar.mount($('body'));
-  
-    // Set the first item as active
-    navbar.setActive(0);
-  });
+(function($) {
+    $.fn.createSidebar = function() {
+        const sidebarHtml = `
+            <nav class="sidebar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li><a href="/home"><img src="/assets/navigations/home.svg"/>Home</a></li>
+                        <li><a href="../menu/menu/"><img src="/assets/navigations/menu.svg"/>Menu</a></li>
+                        <li><a href="#"><img src="/assets/navigations/order.svg"/>Orders</a></li>
+                        <li><a href="#"><img src="/assets/navigations/checkouts.svg"/>Checkouts</a></li>
+                        <li><a href="#"><img src="/assets/navigations/settings.svg"/>Settings</a></li>
+                    </ul>
+                </div>
+            </nav>`;
+        
+        this.html(sidebarHtml);
+        return this;
+    };
+})(jQuery);
